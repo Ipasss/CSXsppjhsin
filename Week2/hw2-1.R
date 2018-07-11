af845b359beb418da3a22c5e746da953087584ad
Cindy <cindy@Cindyde-MacBook-Pro.local>
2018-07-11 11:34:50 +0800

    added week

---
+++ Week2/hw2-1.R
@@ -0,0 +1,19 @@
+#install.packages("ggplot2")
+library(ggplot2)
+
+iris
+
+ggplot(data = iris, aes(x = Species)) + geom_bar()
+
+ggplot(data = iris, aes(x = Sepal.Length)) + geom_histogram()
+
+ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width)) + geom_point()
+
+ggplot(data = iris, aes(x = Species, y = Sepal.Length)) + geom_boxplot()
+
+library(ggplot2)
+library(GGally)
+library(scales)
+library(memisc)
+
+ggpairs(iris)
